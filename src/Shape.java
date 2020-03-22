2b41cbcae4c967a5feaa779432a24f9cdb90df8f
Alessia Pizzoccheri <pizzoccheri.alessia5@gmail.com>
2020-03-22 15:55:55 -0400

    created Shape interface

---
+++ src/Shape.java
@@ -0,0 +1,12 @@
+/**
+ * This is a Shape interface. It includes all methods available to
+ * a shape.
+ */
+public interface Shape {
+
+  int getWidth();
+
+  int getHeight();
+
+  String getName();
+}
