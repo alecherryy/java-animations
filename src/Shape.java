cec3367b1232cd5df296a14dbbd47b5156ff101a
Alessia Pizzoccheri <pizzoccheri.alessia5@gmail.com>
2020-03-22 20:01:29 -0400

    created shape interface and class

--- src/Shape.java
+++ src/Shape.java
@@ -4,9 +4,41 @@
  */
 public interface Shape {
 
+  /**
+   * Returns the width of the shape.
+   *
+   * @return the width of the shape
+   */
   int getWidth();
 
+  /**
+   * Returns the height of the shape.
+   *
+   * @return the width of the shape
+   */
   int getHeight();
 
-  String getName();
+  /**
+   * Returns the size of the Shape in the form
+   * of value pair (width, height).
+   *
+   * @return the size of the shape
+   */
+  Pair getSize();
+
+  /**
+   * Returns the position of the Shape in the form
+   * of value pair coordinates (x, y).
+   *
+   * @return the position of the shape
+   */
+  Pair getPosition();
+
+  /**
+   * Returns a summery of the Shape in a string; meaning, a detailed
+   * description of the shape information and its expected behavior.
+   *
+   * @return a summary in a string
+   */
+  String toString();
 }
---
+++ src/ShapeImpl.java
@@ -0,0 +1,25 @@
+/**
+ * This class represents a Shape, it implements all the
+ * methods that belong to the
+ */
+public class ShapeImpl implements Shape {
+  @Override
+  public int getWidth() {
+    return 0;
+  }
+
+  @Override
+  public int getHeight() {
+    return 0;
+  }
+
+  @Override
+  public Pair getSize() {
+    return null;
+  }
+
+  @Override
+  public Pair getPosition() {
+    return null;
+  }
+}
