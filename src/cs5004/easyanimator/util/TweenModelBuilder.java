3955367cbb407ca30c981520f6643068996fd003
Alessia Pizzoccheri <pizzoccheri.alessia5@gmail.com>
2020-03-31 20:11:32 -0400

    added view package

---
+++ src/cs5004/easyanimator/util/TweenModelBuilder.java
@@ -0,0 +1,35 @@
+package cs5004.easyanimator.util;
+
+
+public interface TweenModelBuilder<T> {
+
+  TweenModelBuilder<T> addOval(
+      String name,
+      float x, float y,
+      float xRadius, float yRadius,
+      float red, float green, float blue,
+      int appear, int disappear);
+
+  TweenModelBuilder<T> addRectangle(
+      String name,
+      float minX, float minY,
+      float width, float height,
+      float red, float green, float blue,
+      int appear, int disappear);
+
+  TweenModelBuilder<T> addMove(
+      String name,
+      float fromX, float fromY, float toX, float toY,
+      int start, int end);
+
+  TweenModelBuilder<T> addColorChange(
+      String name,
+      float oldR, float oldG, float oldB, float newR, float newG, float newB,
+      int start, int end);
+
+  TweenModelBuilder<T> addScaleChange(String name, float fromSx, float
+      fromSy, float toSx, float toSy, int start, int end);
+
+  T build();
+
+}
---
+++ src/cs5004/easyanimator/view/SVGView.java
@@ -0,0 +1,13 @@
+package cs5004.easyanimator.view;
+
+import java.util.List;
+
+import cs5004.easyanimator.model.animations.Animations;
+import cs5004.easyanimator.model.shapes.Shape;
+
+public class SVGView extends TextualView{
+
+  public SVGView(double tick, List<Shape> shapes, List<Animations> animations) {
+    super(tick, shapes, animations);
+  }
+}
