25062aeb8847c6d0e56e802fd35a74baaf04d3db
Alessia Pizzoccheri <pizzoccheri.alessia5@gmail.com>
2020-04-12 14:06:42 -0400

    re-added changes

--- src/cs5004/easyanimator/model/AnimationModelImpl.java
+++ src/cs5004/easyanimator/model/AnimationModelImpl.java
@@ -1,6 +1,6 @@
 package cs5004.easyanimator.model;
 
-import java.awt.Color;
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -84,19 +84,18 @@ public class AnimationModelImpl implements AnimationModel {
    *
    * @param name of the shape to remove
    */
-  public AnimationModel removeShape(String name) {
-    for (Shapes s : this.shapes) {
-      if (s.getName().equals(name)) {
+  public void removeShape(String name) {
+    for (int i = 0; i < this.shapes.size(); i++) {
+      if (this.shapes.get(i).getName().equals(name)) {
         // remove all animations associated with the shape
-        for (Animations a : this.animations) {
-          if (a.getShape().getName().equals(s.getName())) {
-            this.animations.remove(a);
+        for (int j = 0; j < this.animations.size(); j++) {
+          if (this.animations.get(j).getShape().getName().equals(this.shapes.get(i).getName())) {
+            this.animations.remove(j);
           }
         }
-        this.shapes.remove(s);
+        this.shapes.remove(i);
       }
     }
-    return this;
   }
 
   /**
@@ -545,7 +544,7 @@ public class AnimationModelImpl implements AnimationModel {
           if (el.get(1) != el.get(9) || el.get(2) != el.get(10)) {
             addMove(k, el.get(1), el.get(2), el.get(9), el.get(10), startT, endT);
           }
-          else if (color != newColor) {
+          if (color != newColor) {
             addColorChange(k, color, newColor, startT, endT);
           }
           if (width != el.get(11) || height != el.get(12)) {
